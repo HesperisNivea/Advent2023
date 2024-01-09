1826fd38d630856e42849dd1be8c48897b80abad
HesperisNivea <143076071+HesperisNivea@users.noreply.github.com>
2024-01-09 10:48:16 +0100

    Add project files.

---
+++ AdventDay1.cs
@@ -0,0 +1,43 @@
+﻿namespace Advent2023;
+
+public static class AdventDay1
+{
+    
+    // check all lines 
+    // save all numbers in list 
+    //add to numbers (first and last)
+    // add them all 
+    public static void Day1()
+    {
+        long total = 0;
+
+        while (Console.ReadLine() is { } line)
+        {
+            if (line != "")
+            {
+                var firstDigit = line.First(l => char.IsDigit(l)) - '0';
+            
+                var lastDigit = line.Last(l => char.IsDigit(l)) - '0';
+
+            
+                var fullNumber = firstDigit * 10 + lastDigit;
+            
+                total += fullNumber;
+
+            }
+            else
+            {
+                break;
+            }
+            
+        }
+
+        Console.WriteLine(total);
+    }
+
+    public static void Day2()
+    {
+
+    }
+    
+}                    
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Advent2023;
+
+//AdventDay1.Day1();
+
+Day10.CreateA2DArray();
+
+Console.ReadKey();
